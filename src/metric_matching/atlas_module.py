fix variance

src/metric_matching/atlas_module.py
@@ -354,9 +354,6 @@ class AtlasMetricModule(L.LightningModule):
         aux = self._compute_outputs(images, epsilon, white_noise)
         nll, aux = self._compute_nll(images, epsilon, aux)
 
-        _, c, h, w = images.shape
-        data_dim = c * h * w
-
         metrics = {
             "nll": nll.mean().detach(),
             "denoising_mse_term": aux["denoising_mse_term"].mean().detach(),
@@ -368,7 +365,7 @@ class AtlasMetricModule(L.LightningModule):
             "projection_log_var": aux["projection_log_var"].detach(),
             "refinement_log_var": aux["refinement_log_var"].detach(),
             "tangent_dim": aux["mask"].float().sum(dim=1).mean().detach(),
-            "latent_var": aux["std"].square().sum(dim=1).mean().div(aux["projection_log_var"] * data_dim).detach(),
+            "latent_var": aux["std"].square().mean().mul(aux["projection_log_var"].exp()).detach(),
         }
         return nll.mean(), metrics
 